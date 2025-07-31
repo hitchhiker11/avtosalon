4db0ea890c884478aefd704c930222359241f237
hitchhiker11 <aybormotov343@gmail.com>
2025-08-01 00:57:28 +0300

    size change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import tunnel from 'astro-tunnel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    tunnel({
+      url: 'http://localhost:4321',
+      port: 4321,
+      acceptCloudflareNotice: true,
+    })
+  ],
   output: 'static',
   build: {
     inlineStylesheets: 'auto'
